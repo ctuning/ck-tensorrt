87f66d0972be4ecd1e9c985dc119879108d195dd
Anton Lokhmotov <anton@dividiti.com>
2016-12-20 16:04:50 +0000

    Print relevant CK env vars.

--- program/imagenet-console/imagenet-console.cpp
+++ program/imagenet-console/imagenet-console.cpp
@@ -12,11 +12,42 @@
 // main entry point
 int main( int argc, char** argv )
 {
-	printf("imagenet-console\n  args (%i):  ", argc);
-	
+	// print environment variables set by CK
+	printf("imagenet-console\n  ck-env:\n");
+
+	const char * caffe_model = "CK_CAFFE_MODEL";
+	const char * caffe_model_value = getenv(caffe_model);
+	printf("    %s=\"%s\"\n", caffe_model, caffe_model_value ? caffe_model_value : "?");
+
+	const char * caffe_weights = "CK_ENV_MODEL_CAFFE_WEIGHTS";
+	const char * caffe_weights_value = getenv(caffe_weights);
+	printf("    %s=\"%s\"\n", caffe_weights, caffe_weights_value ? caffe_weights_value : "?");
+
+	const char * imagenet_mean_bin = "CK_CAFFE_IMAGENET_MEAN_BIN";
+	const char * imagenet_mean_bin_value = getenv(imagenet_mean_bin);
+	printf("    %s=\"%s\"\n", imagenet_mean_bin, imagenet_mean_bin_value ? imagenet_mean_bin_value : "?");
+
+	const char * imagenet_synset_words_txt = "CK_CAFFE_IMAGENET_SYNSET_WORDS_TXT";
+	const char * imagenet_synset_words_txt_value = getenv(imagenet_synset_words_txt);
+	printf("    %s=\"%s\"\n", imagenet_synset_words_txt, imagenet_synset_words_txt_value ? imagenet_synset_words_txt_value : "?");
+
+	const char * caffe_iterations = "CK_CAFFE_ITERATIONS";
+	const char * caffe_iterations_value = getenv(caffe_iterations);
+	printf("    %s=%s\n", caffe_iterations, caffe_iterations_value ? caffe_iterations_value : "?");
+
+	const char * caffe_batch_size = "CK_CAFFE_BATCH_SIZE";
+	const char * caffe_batch_size_value = getenv(caffe_batch_size);
+	printf("    %s=%s\n", caffe_batch_size, caffe_batch_size_value ? caffe_batch_size_value : "?");
+
+	printf("\n\n");
+
+
+	// print command line arguments
+	printf("imagenet-console\n  args (%i):", argc);
+
 	for( int i=0; i < argc; i++ )
-		printf("%i [%s]  ", i, argv[i]);
-		
+		printf("\n    [%i] %s", i, argv[i]);
+
 	printf("\n\n");
 	
 	
