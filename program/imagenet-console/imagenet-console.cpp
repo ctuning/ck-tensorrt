d10ccad3209e2fd139d4f7b66cefc33ff68b0f87
Anton Lokhmotov <anton@dividiti.com>
2016-12-21 11:15:32 +0000

    Assign CK env vars to vals.

--- program/imagenet-console/imagenet-console.cpp
+++ program/imagenet-console/imagenet-console.cpp
@@ -15,33 +15,33 @@ int main( int argc, char** argv )
 	// print environment variables set by CK
 	printf("imagenet-console\n  ck-env:\n");
 
-	const char * caffe_model = "CK_CAFFE_MODEL";
-	const char * caffe_model_value = getenv(caffe_model);
-	printf("    %s=\"%s\"\n", caffe_model, caffe_model_value ? caffe_model_value : "?");
+	const char * caffe_model_var = "CK_CAFFE_MODEL";
+	const char * caffe_model_val = getenv(caffe_model_var);
+	printf("    %s=\"%s\"\n", caffe_model_var, caffe_model_val ? caffe_model_val : "?");
 
-	const char * caffe_weights = "CK_ENV_MODEL_CAFFE_WEIGHTS";
-	const char * caffe_weights_value = getenv(caffe_weights);
-	printf("    %s=\"%s\"\n", caffe_weights, caffe_weights_value ? caffe_weights_value : "?");
+	const char * caffe_weights_var = "CK_ENV_MODEL_CAFFE_WEIGHTS";
+	const char * caffe_weights_val = getenv(caffe_weights_var);
+	printf("    %s=\"%s\"\n", caffe_weights_var, caffe_weights_val ? caffe_weights_val : "?");
 
-	const char * imagenet_mean_bin = "CK_CAFFE_IMAGENET_MEAN_BIN";
-	const char * imagenet_mean_bin_value = getenv(imagenet_mean_bin);
-	printf("    %s=\"%s\"\n", imagenet_mean_bin, imagenet_mean_bin_value ? imagenet_mean_bin_value : "?");
+	const char * imagenet_mean_bin_var = "CK_CAFFE_IMAGENET_MEAN_BIN";
+	const char * imagenet_mean_bin_val = getenv(imagenet_mean_bin_var);
+	printf("    %s=\"%s\"\n", imagenet_mean_bin_var, imagenet_mean_bin_val ? imagenet_mean_bin_val : "?");
 
-	const char * imagenet_synset_words_txt = "CK_CAFFE_IMAGENET_SYNSET_WORDS_TXT";
-	const char * imagenet_synset_words_txt_value = getenv(imagenet_synset_words_txt);
-	printf("    %s=\"%s\"\n", imagenet_synset_words_txt, imagenet_synset_words_txt_value ? imagenet_synset_words_txt_value : "?");
+	const char * imagenet_synset_words_txt_var = "CK_CAFFE_IMAGENET_SYNSET_WORDS_TXT";
+	const char * imagenet_synset_words_txt_val = getenv(imagenet_synset_words_txt_var);
+	printf("    %s=\"%s\"\n", imagenet_synset_words_txt_var, imagenet_synset_words_txt_val ? imagenet_synset_words_txt_val : "?");
 
-	const char * imagenet_val_txt = "CK_CAFFE_IMAGENET_VAL_TXT";
-	const char * imagenet_val_txt_value = getenv(imagenet_val_txt);
-	printf("    %s=\"%s\"\n", imagenet_val_txt, imagenet_val_txt_value ? imagenet_val_txt_value : "?");
+	const char * imagenet_val_txt_var = "CK_CAFFE_IMAGENET_VAL_TXT";
+	const char * imagenet_val_txt_val = getenv(imagenet_val_txt_var);
+	printf("    %s=\"%s\"\n", imagenet_val_txt_var, imagenet_val_txt_val ? imagenet_val_txt_val : "?");
 
-	const char * caffe_iterations = "CK_CAFFE_ITERATIONS";
-	const char * caffe_iterations_value = getenv(caffe_iterations);
-	printf("    %s=%s\n", caffe_iterations, caffe_iterations_value ? caffe_iterations_value : "?");
+	const char * caffe_iterations_var = "CK_CAFFE_ITERATIONS";
+	const char * caffe_iterations_val = getenv(caffe_iterations_var);
+	printf("    %s=%s\n", caffe_iterations_var, caffe_iterations_val ? caffe_iterations_val : "?");
 
-	const char * caffe_batch_size = "CK_CAFFE_BATCH_SIZE";
-	const char * caffe_batch_size_value = getenv(caffe_batch_size);
-	printf("    %s=%s\n", caffe_batch_size, caffe_batch_size_value ? caffe_batch_size_value : "?");
+	const char * caffe_batch_size_var = "CK_CAFFE_BATCH_SIZE";
+	const char * caffe_batch_size_val = getenv(caffe_batch_size_var);
+	printf("    %s=%s\n", caffe_batch_size_var, caffe_batch_size_val ? caffe_batch_size_val : "?");
 
 	printf("\n\n");
 
@@ -67,10 +67,10 @@ int main( int argc, char** argv )
 
 	// create imageNet
 	imageNet* net = imageNet::Create(
-				caffe_model_value,
-				caffe_weights_value,
-				imagenet_mean_bin_value,
-				imagenet_synset_words_txt_value
+				caffe_model_val,
+				caffe_weights_val,
+				imagenet_mean_bin_val,
+				imagenet_synset_words_txt_val
 				);
 
 	if( !net )
